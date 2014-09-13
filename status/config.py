362095eaecb102f30e2778506481e27586e8d252
Alexandre <arekkusu@users.noreply.github.com>
2014-09-13 23:17:59 +0200

    Create config.py

---
+++ status/config.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+JSON_FILENAME = '/data/www_app/status/json/ptl_space_api.json'
+JSON_FILE_REL = '/json/ptl_space_api.json'
+ROOT_FOLDER = '/data/www_app/status/'
+
+#API_KEYS must be a fixed lenght, one per line
+KEY_LENGTH = 20
+API_KEYS = '/data/www_app/status/api_keys'
