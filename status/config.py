a80318dedbba7e1b6c79bb2f6df13e7a6078958d
arekkusu <arekkusu@users.noreply.github.com>
2014-09-14 13:17:26 +0200

    Used autopep8 to normalize / cleanup code

--- status/config.py
+++ status/config.py
@@ -4,6 +4,6 @@ JSON_FILENAME = '/data/www_app/status/json/ptl_space_api.json'
 JSON_FILE_REL = '/json/ptl_space_api.json'
 ROOT_FOLDER = '/data/www_app/status/'
 
-#API_KEYS must be a fixed lenght, one per line
+# API_KEYS must be a fixed lenght, one per line
 KEY_LENGTH = 20
 API_KEYS = '/data/www_app/status/api_keys'
